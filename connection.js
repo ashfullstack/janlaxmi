b5d3d8b15d2220c3a8162c7d307fb05cacfafc86
ashfullstack <akashramsonule@gmail.com>
2024-12-18 14:24:24 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 var util = require("util");
 
 var conn = mysql.createConnection({
-    "host": "localhost",
-    "user": "root",
-    "password": "",
-    "database": "janlakshmi_miltistate"
+    "host": "bsutls7gqwbxyfnqgovv-mysql.services.clever-cloud.com",
+    "user": "uubqsthcv5mbyayb",
+    "password": "tfc8PNxukG7FicPB2zBf",
+    "database": "bsutls7gqwbxyfnqgovv"
 })
 
 var exe = util.promisify(conn.query).bind(conn);
